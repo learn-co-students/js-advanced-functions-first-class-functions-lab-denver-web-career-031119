a202f9816c9280a227d1ba4d780325f25d040316
github username <github email address>
2019-04-23 17:17:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  const firstTwo = [drivers[0], drivers[1]];
+  return firstTwo;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  const lastTwo = [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+  return lastTwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {return multiplier * fare;};
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, firstOrLast) {
+  return firstOrLast(drivers);
+}
